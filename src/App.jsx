3428a3306e4dd6b822a1618a981b8d4f33c0f488
小川裕太 <oytkt0510@gmail.com>
2020-05-31 13:14:59 +0900

    チャットボット　完成
    React Hooks 適用

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,122 @@
-import React from 'react';
-import defaultDataset from "./dataset";
+import React, {useState, useEffect, useCallback} from 'react';
 import './assets/styles/style.css'
-import {AnswersList, Chats} from "./components/index"
+import {AnswersList, Chats, } from "./components/index"
+import FormDialog from './components/forms/FormDialog'
+import {db} from './firebase/index'
 
 
-export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      answers: [],
-      chats: [],
-      currentId: "init",
-      dataset: defaultDataset,
-      open: false
-    }
-    this.selectedAnswer = this.selectAnswer.bind(this)
-  }
 
-  displayNextQuestion = (nextQuestionId) =>{
-    const chats = this.state.chats
-    chats.push({
-      text: this.state.dataset[nextQuestionId].question,
-      type: 'question'
-    })
-this.setState({
-  answers: this.state.dataset[nextQuestionId].answers,
-  chats: chats,
-  currentId: nextQuestionId
 
-})
+const App = () => {
+    const [answers, setAnswers] = useState([]);            // 回答コンポーネントに表示するデータ
+    const [chats, setChats] = useState([]);                // チャットコンポーネントに表示するデータ
+    const [currentId, setCurrentId] = useState('init');    // 現在の質問ID
+    const [dataset, setDataset] = useState({});            // 質問と回答のデータセット
+    const [open, setOpen] = useState(false);               // 問い合わせフォーム用モーダルの開閉を管理
 
-  }
+    // 問い合わせフォーム用モーダルを開くCallback関数
+    const handleOpen = useCallback(() => {
+        setOpen(true)
+    },[setOpen]);
 
-  selectAnswer = (selectedAnswer, nextQuestionId) => {
-    switch(true) {
-      case(nextQuestionId === 'init'):
-      this.displayNextQuestion(nextQuestionId)
-        break;
-      default:
-        const chats = this.state.chats;
-        chats.push({
-          text: selectedAnswer,
-          type: 'answer'
+    // 問い合わせフォーム用モーダルを閉じるCallback関数
+    const handleClose = useCallback(() => {
+        setOpen(false)
+    },[setOpen]);
+
+    // 新しいチャットを追加するCallback関数
+    const addChats = useCallback((chat) => {
+        setChats(prevChats => {
+            return [...prevChats, chat]
         })
-    
-        this.setState({
-          chats: chats
-        })
+    },[setChats]);
 
-        this.displayNextQuestion(nextQuestionId)
-        break;
+    // 次の質問をチャットエリアに表示する関数
+    const displayNextQuestion = (nextQuestionId, nextDataset) => {
+        // 選択された回答と次の質問をチャットに追加
+        addChats({
+            text: nextDataset.question,
+            type: 'question'
+        });
+
+        // 次の回答一覧をセット
+        setAnswers(nextDataset.answers)
+
+        // 現在の質問IDをセット
+        setCurrentId(nextQuestionId)
     }
-  }
 
-  
- 
+    // 回答が選択された時に呼ばれる関数
+    const selectAnswer = (selectedAnswer, nextQuestionId) => {
+        switch (true) {
+            // お問い合わせが選択された場合
+            case (nextQuestionId === 'contact'):
+                handleOpen();
+                break;
 
-  componentDidMount() {
-    const initAnswer = "";
-    this.selectedAnswer(initAnswer, this.state.currentId)
-  }
+            // リンクが選択された時
+            case /^https:*/.test(nextQuestionId):
+                const a = document.createElement('a');
+                a.href = nextQuestionId;
+                a.target = '_blank';
+                a.click();
+                break;
+
+            // 選択された回答をchatsに追加
+            default:
+                // 現在のチャット一覧を取得
+                addChats({
+                    text: selectedAnswer,
+                    type: 'answer'
+                })
+
+                setTimeout(() => displayNextQuestion(nextQuestionId, dataset[nextQuestionId]), 750)
+                break;
+        }
+    };
+
+    // 最初の質問をチャットエリアに表示する
+    useEffect(() => {
+        (async() => {
+            const initDataset = {};
+
+            // Fetch questions dataset from Firestore
+            await db.collection('questions').get().then(snapshots => {
+                snapshots.forEach(doc => {
+                    initDataset[doc.id] = doc.data()
+                })
+            });
+
+            // Firestoreから取得したデータセットを反映
+            setDataset(initDataset);
+
+            // 最初の質問を表示
+            displayNextQuestion(currentId, initDataset[currentId])
+        })();
+    }, []);
+
+    // 最新のチャットが見えるように、スクロール位置の頂点をスクロール領域の最下部に設定する
+    useEffect(() => {
+        const scrollArea = document.getElementById('scroll-area');
+        if (scrollArea) {
+            scrollArea.scrollTop = scrollArea.scrollHeight;
+        }
+    });
     
   
-  render() {
+  
   return (
     <div>
       <section className="c-section">
         <div className="c-box">
-          <Chats chats={this.state.chats}/>
-          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
+          <Chats chats={chats}/>
+          <AnswersList answers={answers} select={selectAnswer} />
+          <FormDialog open={open} handleClose={handleClose} />
         </div>
       </section>
     </div>
   );
   }
-}
 
+
+export default App
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import {Chat} from "./index";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-      maxWidth: '36ch',
-      backgroundColor: theme.palette.background.paper,
-    },
-    
-  }));
+const useStyles = makeStyles((theme) => (
+  createStyles({
+    "chats": {
+      height: 400,
+      padding: '0',
+      overflow: 'auto'
+    }
+  })
+  ));
 
 
 
@@ -19,7 +20,7 @@ const Chats = (props) => {
 
     return (
 
-        <List className={classes.root}>
+        <List className={classes.chats} id={"scroll-area"}>
             {props.chats.map((chat, index) => {
                 return <Chat text={chat.text} type={chat.type} key={index.toString()} />
         })}
